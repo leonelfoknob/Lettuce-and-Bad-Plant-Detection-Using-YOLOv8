7a293e812e9706be9c2142d870704366ad0bde5c
Leonel <83438309+leonelfoknob@users.noreply.github.com>
2024-04-28 21:19:11 +0300

    Update custom_dataset_yolo_train.py

--- custom_dataset_yolo_train.py
+++ custom_dataset_yolo_train.py
@@ -1,10 +1,3 @@
-'''from ultralytics import YOLO
-
-# Load a model
-model = YOLO("yolov8n.yaml")
-
-model.train(data="config.yaml", epochs=3)  # train the model
-'''
 from ultralytics import YOLO
  
 # Load the model.
