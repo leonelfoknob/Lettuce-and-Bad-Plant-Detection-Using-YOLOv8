ed617a5e1ca948e6a616534492c07a143a80b24c
Leonel <83438309+leonelfoknob@users.noreply.github.com>
2024-04-28 21:18:38 +0300

    Update custom_data_predict.py

--- custom_data_predict.py
+++ custom_data_predict.py
@@ -32,7 +32,7 @@ while ret:
             cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
             cv2.putText(frame, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
                         cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
-    #out.write(frame)
+    #out.write(frame) #this line is to save video_file_result
     cv2.imshow('frame', frame) 
     ret, frame = cap.read()
     if cv2.waitKey(1) & 0xFF == ord('q'):
